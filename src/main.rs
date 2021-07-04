c5a5c565e2b9ac483f77c53254ffe1e821d8ae7e
Sergei Belokon <srj.belokon@gmail.com>
2021-07-05 00:40:37 +0300

    Added /gcd route to the server thing

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
-    println!("Serving on http://localhost:3000...");
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_index))
+    });
 
+    println!("Serving on http://localhost:3000...");
     server
         .bind("127.0.0.1:3000")
         .expect("error binding server to address")
